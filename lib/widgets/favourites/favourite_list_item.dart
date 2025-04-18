acf726128b7969f42a002903674deb83332382f6
Isabel Agbu <isabelxcoding@gmail.com>
2025-04-18 13:58:40 -0700

    fixed bug in favourites screen

--- lib/widgets/favourites/favourite_list_item.dart
+++ lib/widgets/favourites/favourite_list_item.dart
@@ -1,96 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:givenget/models/donation_item.dart';
 import 'package:givenget/screens/main_screens/explore/donation_detail_screen.dart';
+import 'package:givenget/services/user_service.dart';
+import 'package:givenget/models/user.dart';
 
-class FavouriteDonationGridListItem extends StatelessWidget {
-  List<DonationItem> favouriteItems;
-  int index;
+class FavouriteDonationGridListItem extends StatefulWidget {
+  final List<DonationItem> favouriteItems;
+  final int index;
   final Function(BuildContext, DonationItem) removeFromFavourites;
   final VoidCallback refreshFavourites;
 
-  FavouriteDonationGridListItem({super.key, required this.favouriteItems, required this.index, required this.removeFromFavourites, required this.refreshFavourites});
+  const FavouriteDonationGridListItem({
+    super.key,
+    required this.favouriteItems,
+    required this.index,
+    required this.removeFromFavourites,
+    required this.refreshFavourites,
+  });
+
+  @override
+  State<FavouriteDonationGridListItem> createState() => _FavouriteDonationGridListItemState();
+}
+
+class _FavouriteDonationGridListItemState extends State<FavouriteDonationGridListItem> {
+  String donorName = '...';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDonorName();
+  }
+
+  Future<void> fetchDonorName() async {
+    final user = await UserService().fetchUserById(widget.favouriteItems[widget.index].donor);
+    setState(() {
+      donorName = user?.name ?? 'Unknown';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final item = widget.favouriteItems[widget.index];
+
     return GestureDetector(
       onTap: () {
-          // we navigate to the detail page with the selected item
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DonationDetailScreen(
-                item: favouriteItems[index],
-                refreshFavorites: refreshFavourites,
-              ),
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DonationDetailScreen(
+              item: item,
+              refreshFavorites: widget.refreshFavourites,
             ),
-          );
-      },  // we navigate to detail screen here
+          ),
+        );
+      },
       child: Card(
         elevation: 3,
         child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.white
-            ),       
-            child: Stack(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, 
-                  children: [
-                    Container(
-                      height: 156,  
-                      width: double.infinity, 
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.network(
-                          favouriteItems[index].imageUrl,
-                          fit: BoxFit.fill, 
-                        ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 156,
+                    width: double.infinity,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(14),
+                      child: Image.network(
+                        item.imageUrl,
+                        fit: BoxFit.fill,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,  
-                        children: [
-                          Text(
-                            favouriteItems[index].title,
-                            textAlign: TextAlign.left,  
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Donated by ' + favouriteItems[index].donor,
-                            style: TextStyle(fontSize: 12),
-                          ),
-                          Text(
-                            'Posted ' + favouriteItems[index].datePosted,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(  
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(6),
-                        color: const Color.fromARGB(104, 0, 0, 0),
-                      ),           
-                      width: 30,
-                      height: 30,
-                      child: IconButton(onPressed: () {                      
-                        removeFromFavourites(context, favouriteItems[index]);
-                      }, icon: Icon(Icons.clear, color: Colors.white, size: 14,),),
                     ),
                   ),
-                )
-              ],
-            )
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Donated by $donorName',
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          'Posted ${item.datePosted}',
+                          style: const TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color.fromARGB(104, 0, 0, 0),
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        widget.removeFromFavourites(context, item);
+                      },
+                      icon: const Icon(Icons.clear, color: Colors.white, size: 14),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
